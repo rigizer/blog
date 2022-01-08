963bff154bb4f5fcabdfd8e55a1294028a362f0d
rigizer <rigizer@gmail.com>
2022-01-08 21:50:54 +0900

    기본 틀 적용, 라우팅 기능 추가

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');      // POST 요청 처리
+const app = express();                          // Node.js Express
+
+const expressErrorHandler = require('express-error-handler');
+const errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html', 
+        '500': './public/500.html'
+    }
+});
+
+// Other Settings
+app.set('view engine', 'pug');                          // 템플릿 엔진 설정
+app.set('views', __dirname + '/views');                 // __dirname 시스템 변수: request.getContextPath();
+app.use(bodyParser.urlencoded({extended: true}));       // POST 요청을 처리하는 기능(미들웨어)을 확장
+
+// Routes
+app.use('/', require('./routes/home/home'));
+app.use('/test', require('./routes/test/test'));
+app.use('/admin', require('./routes/admin/admin'));
+
+app.use(expressErrorHandler.httpError(404));
+app.use(expressErrorHandler.httpError(500));
+app.use(errorHandler);
+
+// Server Port Setting
+const port = 8000;
+app.listen(port, function() {
+    console.log('SERVER ON! PORT: ' + port);
+});
