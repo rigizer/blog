630e891755afe63011a916d3e322e96b076a0df2
rigizer <rigizer@gmail.com>
2022-01-09 11:15:28 +0900

    라우터 화살표함수 적용

--- routes/test/testRouter.js
+++ routes/test/testRouter.js
@@ -7,11 +7,11 @@ const query = require('../../config/db/Query').query;
 // ERROR: await is only valid in async functions and the top level bodies of modules
 // https://codingmania.tistory.com/552 참고
 
-router.get('/', async function(req, res) {
+router.get('/', async (req, res) => {
     let sql = query.SELECT_MWS_TEST;
     let args = [];
     
-    let result = await executeQuery(sql, args, function(err, result) {
+    let result = await executeQuery(sql, args, (err, result) => {
         if (err) {
             console.log('QUERY ERROR: ' + err);
             res.end();
@@ -21,6 +21,10 @@ router.get('/', async function(req, res) {
     let model = {};
     model.test = result;
     res.render('test/main', {model: model});
+});
+
+router.get('/bootstrapTest', (req, res) => {
+    res.render('test/bootstrapTest');
 });
 
 module.exports = router;
